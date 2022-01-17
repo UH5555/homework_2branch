b72e83512ed89111ccb72cf9931bee86d132b8d8
UH5555 <zhuhao162@163.com>
2022-01-17 20:35:26 +0800

    homework_for

---
+++ test_0117/homework.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+////数1-100中9的个数
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 10 == 9)
+//		{
+//			printf("%d ", i);
+//			count++;
+//		}
+//		if (i / 10 == 9)
+//		{
+//			printf("%d ", i);
+//			count++;
+//		}
+//	}
+//	printf("\n9的个数是：%d", count);
+//	return 0;
+//}
