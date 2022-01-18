38fbdabd80f12ea40aac8cb4e082eec6eadc48c3
UH5555 <zhuhao162@163.com>
2022-01-18 21:01:58 +0800

    2.0

--- test_0117/homework.c
+++ test_0117/homework.c
@@ -22,3 +22,53 @@
 //	printf("\n9的个数是：%d", count);
 //	return 0;
 //}
+
+//1-1/2+1/3-1/4+...-1/100
+//int main()
+//{
+//	int i = 0;
+//	double sum = 0.0;
+//	for (i = 1; i <= 100; i += 2)
+//	{
+//		sum += 1.0 / i;
+//	}
+//	for (i = 2; i <= 100; i += 2)
+//	{
+//		sum -= 1.0 / i;
+//	}
+//	printf("%lf\n", sum);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	double sum = 0.0;
+//	int flag = 1;
+//	for (i = 1; i <= 100; i++)
+//	{
+//			sum += flag * 1.0 / i;
+//			flag = -flag;
+//	}
+//	printf("%lf\n", sum);
+//	return 0;
+//}
+
+//找到一个数组中最大的数
+//int main()
+//{
+//	int arr[] = {-1,-2,-3,-4,-5,-6,-7,-8,-9,-10 };
+//	//int max = 0;
+//	int max = arr[0];
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 1; i < sz; i++)
+//	{
+//		if (arr[i] > max)
+//		{
+//			max = arr[i];
+//		}
+//	}
+//	printf("max=%d\n", max);
+//	return 0;
+//}
