371da1de3fe91d91b07339b8eda6e3fd171b9d2c
UH5555 <zhuhao162@163.com>
2022-01-19 20:37:38 +0800

    homework_for(all)

--- test_0117/homework.c
+++ test_0117/homework.c
@@ -72,3 +72,19 @@
 //	printf("max=%d\n", max);
 //	return 0;
 //}
+
+//在屏幕上输出9*9乘法口诀表
+int main()
+{
+	int i = 0;
+	for (i = 1; i <= 9; i++)
+	{
+		int j = 1;
+		for (j = 1; j <= i; j++)
+		{
+			printf("%d*%d=%-2d ", i, j, i * j);//%2d，输出两位数。（右对齐）；%-2d（左对齐）
+		}
+		printf("\n");
+	}
+	return 0;
+}
